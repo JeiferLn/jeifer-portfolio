363ec61b7cb804d8948ab5b7614dd73ffa4421e3
Jeifer Leon <jeiferleon@gmail.com>
2025-03-10 23:24:37 -0500

    [FIX] Typos

--- src/components/about/type-about/GeneralInformation.tsx
+++ src/components/about/type-about/GeneralInformation.tsx
@@ -12,7 +12,7 @@ function GeneralInformation({
   dataInformation: IGeneralInformation;
 }) {
   return (
-    <div className="w-full h-[240px] lg:h-auto flex flex-col justify-center lg:justify-start bg-container rounded-lg py-4 px-6 text-center lg:text-start">
+    <div className="w-full h-[240px] lg:h-auto flex flex-col justify-center lg:justify-start bg-container rounded-lg py-6 px-6 text-center lg:text-start">
       <h3 className="text-secondary mb-2">{dataInformation.date}</h3>
       <h2 className="text-xl text-primary">{dataInformation.title}</h2>
       <p className="text-primary-description mt-4 text-pretty text-sm">
